6c3eadf1a904d878b07aaf7da3120c416bdcb1a8
Marcello-Marques <137443029+Marcello-Marques@users.noreply.github.com>
2023-08-08 14:53:48 +0000

    for Ernesto's analysis

--- src/app.js
+++ src/app.js
@@ -1,23 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
   //write your code here
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
+  let noun = ["jogger", "racoon", "lastOfUs"];
+  let extensions = ["com", "net,", "us", "io"];
   let phrase = "";
   for (let i = 0; i < pronoun.length; i++) {
     for (let y = 0; y < adj.length; y++) {
       for (let x = 0; x < noun.length; x++) {
-        phrase += `<div>${pronoun[i]}${adj[y]}${noun[x]}.com</div>`;
+        for (let z = 0; z < extensions.length; z++) {
+          let domain;
+          if (noun[x].toLowerCase().endsWith(extensions[z])) {
+            domain = noun[x].replace(`/${extensions[z]}/i`);
+          } else {
+            domain = noun[x];
+          }
+          console.log(`${pronoun[i]}${adj[y]}${domain}.${extensions[z]}`);
+        }
       }
     }
   }
-  const targeHeading = document.querySelector("#excuse");
-  targeHeading.innerHTML = phrase;
-};
+
