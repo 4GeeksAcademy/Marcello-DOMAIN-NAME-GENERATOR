ce96b30d4e726de884e0ef1ac85913fafebe99fa
Marcello-Marques <137443029+Marcello-Marques@users.noreply.github.com>
2023-08-09 02:13:27 +0000

    Extension rule changes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,21 @@
-  //write your code here
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon", "lastOfUs"];
-  let extensions = ["com", "net,", "us", "io"];
-  let phrase = "";
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let y = 0; y < adj.length; y++) {
-      for (let x = 0; x < noun.length; x++) {
-        for (let z = 0; z < extensions.length; z++) {
-          let domain;
-          if (noun[x].toLowerCase().endsWith(extensions[z])) {
-            domain = noun[x].replace(`/${extensions[z]}/i`);
-          } else {
-            domain = noun[x];
-          }
-          console.log(`${pronoun[i]}${adj[y]}${domain}.${extensions[z]}`);
+//write your code here
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon", "lastOfUs"];
+let extensions = ["com", "net", "us", "io"];
+let phrase = "";
+for (let i = 0; i < pronoun.length; i++) {
+  for (let y = 0; y < adj.length; y++) {
+    for (let x = 0; x < noun.length; x++) {
+      for (let z = 0; z < extensions.length; z++) {
+        let domain;
+        if (noun[x].toLowerCase().endsWith(extensions[z])) {
+          domain = noun[x].slice(0, -extensions[z].length);
+        } else {
+          domain = noun[x];
         }
+        console.log(`${pronoun[i]}${adj[y]}${domain}.${extensions[z]}`);
       }
     }
   }
-
+}
